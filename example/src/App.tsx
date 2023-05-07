bd7c561e61d357192024380fae6e91f8de410d2c
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2023-05-07 20:09:52 +0530

    feat: integrate android ocr module with example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,46 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'rn-ocr-lib';
+import {
+  StyleSheet,
+  View,
+  Button,
+  useWindowDimensions,
+  Text,
+} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import RNFetchBlob from 'rn-fetch-blob';
+
+import { ocr } from 'rn-ocr-lib';
+
+const IMAGE_URL: string =
+  'https://chillyfacts.com/wp-content/uploads/2018/11/ocr-sample2.png';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const { width } = useWindowDimensions();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const [text, setText] = useState<string>('');
+
+  const handleOcr = async (): Promise<void> => {
+    try {
+      const fetchRes = await RNFetchBlob.fetch('GET', IMAGE_URL);
+      const base64 = fetchRes.base64();
+      const res = await ocr(base64);
+      setText(res);
+    } catch (err) {}
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <FastImage
+        style={[styles.image, { width }]}
+        source={{
+          uri: IMAGE_URL,
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.contain}
+      />
+      <Button title="Do OCR" onPress={handleOcr} />
+      {text && <Text style={styles.textContent}>{text}</Text>}
     </View>
   );
 }
@@ -21,11 +49,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  image: {
+    height: 180,
+  },
+  textContent: {
+    margin: 12,
   },
 });
