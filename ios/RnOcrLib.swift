e08318f7eea218111e0d8076d84bb04fd6970fd7
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2023-05-16 03:02:30 +0530

    chore: add progress event emitter ios

--- ios/RnOcrLib.swift
+++ ios/RnOcrLib.swift
@@ -10,11 +10,6 @@ class RnOcrLib: RCTEventEmitter {
     return [C_FINISHED_EVENT, C_PROGRESS_EVENT]
   }
 
-  // @objc(multiply:withB:withResolver:withRejecter:)
-  // func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-  //   resolve(a*b)
-  // }
-
   @objc(getText:withOcrInputType:withOcrOptions:withResolver:withRejecter:)
   func getText(data: String, ocrInputType: String, ocrOptions: NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
     let scanImage: UIImage? = ocrInputType == "BASE64" ? convertBase64StringToImage(imageBase64String: data) : UIImage(contentsOfFile: data)
@@ -25,32 +20,33 @@ class RnOcrLib: RCTEventEmitter {
 
     let requestHandler = VNImageRequestHandler(cgImage: cgImage)
 
-    // Create a new request to recognize text.
     let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
     request.recognitionLevel = .accurate
     request.recognitionLanguages = ["en_GB"]
+    request.progressHandler = recognizeProgressHandler(request:progress:error:)
 
     do {
-      // Perform the text-recognition request.
       try requestHandler.perform([request])
       resolve("")
     } catch {
       reject("ocr_error", "Unable to perform the requests: \(error).", nil)
     }
-    // self.sendEvent(withName: "progress", body: ["percent": 10])
-    // resolve("works")
   }
 
   func convertBase64StringToImage (imageBase64String: String) -> UIImage? {
-      if let imageData = Data(base64Encoded: imageBase64String) {
-          let image = UIImage(data: imageData)
-          return image!
-      }
-      
-      return nil
+    if let imageData = Data(base64Encoded: imageBase64String) {
+        let image = UIImage(data: imageData)
+        return image!
+    }
+    
+    return nil
   }
 
-  func recognizeTextHandler(request: VNRequest, error: Error?) {
+  func recognizeProgressHandler(request: VNRequest, progress: Double, error: Error?) -> Void {
+    self.sendEvent(withName: C_PROGRESS_EVENT, body: ["percent": progress * 100])
+  }
+
+  func recognizeTextHandler(request: VNRequest, error: Error?) -> Void {
     guard let observations =
             request.results as? [VNRecognizedTextObservation] else {
       return
