30c91ec968ba3ed207e463d8f5ae9c9233b42698
Naveen Vignesh.B <naveenvignesh5.nv@gmail.com>
2023-05-16 00:24:54 +0530

    chore: add ocr method to ios

--- ios/RnOcrLib.m
+++ ios/RnOcrLib.m
@@ -1,10 +1,18 @@
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(RnOcrLib, NSObject)
+@interface RCT_EXTERN_MODULE(RnOcrLib, RCTEventEmitter)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+// RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+//                  withResolver:(RCTPromiseResolveBlock)resolve
+//                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getText:(NSString *) data
+                 withOcrInputType:(NSString *) ocrInputType
+                 withOcrOptions:(NSDictionary *) ocrOptions
+                 withResolver:(RCTPromiseResolveBlock) resolve
+                 withRejecter:(RCTPromiseRejectBlock) reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/RnOcrLib.swift
+++ ios/RnOcrLib.swift
@@ -1,8 +1,26 @@
-@objc(RnOcrLib)
-class RnOcrLib: NSObject {
+import Foundation
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+@objc(RnOcrLib)
+class RnOcrLib: RCTEventEmitter {
+  let C_FINISHED_EVENT: String = "finished";
+  let C_PROGRESS_EVENT: String = "progress";
+
+  override func supportedEvents() -> [String]! {
+    return [C_FINISHED_EVENT, C_PROGRESS_EVENT]
+  }
+
+  // @objc(multiply:withB:withResolver:withRejecter:)
+  // func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+  //   resolve(a*b)
+  // }
+
+  @objc(getText:withOcrInputType:withOcrOptions:withResolver:withRejecter:)
+  func getText(data: String, ocrInputType: String, ocrOptions: NSDictionary, resolve: RCTPromiseResolveBlock,reject: RCTPromiseRejectBlock) -> Void {
+    print("data", data)
+    print("ocrInputType", ocrInputType)
+    print("ocrInputType", ocrOptions)
+
+    self.sendEvent(withName: "progress", body: ["percent": 10])
+    resolve("works")
   }
 }
